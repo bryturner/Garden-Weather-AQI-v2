3d6ec2817860a17d5e7116b3ebb2063d06dca0fc
bryan.turner.ch@gmail.com <bryan.turner.ch@gmail.com>
2022-02-02 08:57:05 -0700

    show config

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const API_KEY_WEA = 'bca38502a60e9869e3bb916116cb6752';
+export const API_KEY_LOC = '6115d9f1b48319';
+export const URL_WEATHER = 'https://api.openweathermap.org/data/2.5/onecall';
+export const URL_AQI =
+  'https://api.openweathermap.org/data/2.5/air_pollution/forecast';
+export const URL_LOCATION = 'https://ipinfo.io/json?';
