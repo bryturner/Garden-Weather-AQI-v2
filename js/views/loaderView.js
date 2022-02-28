13b26b1c951955ba7a270e12908348838a565cfa
bryan.turner.ch@gmail.com <bryan.turner.ch@gmail.com>
2022-02-27 19:56:04 -0700

    fix loader bug

--- js/views/loaderView.js
+++ js/views/loaderView.js
@@ -2,8 +2,3 @@ export const toggleLoader = function () {
   const loader = document.querySelector('.loader');
   loader.classList.toggle('hide-load');
 };
-
-export const removeLoader = function () {
-  const loader = document.querySelector('.loader');
-  loader.classList.add('hide-load');
-};
